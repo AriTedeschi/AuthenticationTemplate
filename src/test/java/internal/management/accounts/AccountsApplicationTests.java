7f950eadbbec154e5c0fc3f26febed7e33dbe7e0
Ari Tedeschi <arijun2010@hotmail.com>
2024-07-15 21:11:25 -0300

    setup: initialize project

---
+++ src/test/java/internal/management/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package internal.management.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
